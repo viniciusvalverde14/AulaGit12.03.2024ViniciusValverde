e180a0764b99e8c8ec0538abdf086c183bb71c36
valverde <viniciusvalverde.tds2.senai@gmail.com>
2024-03-12 17:15:09 -0300

    troquei me por menst

--- ex02.js
+++ ex02.js
@@ -3,5 +3,5 @@ let n1 = 2;
 // declaro valor de y
 let n2 = 5;
 let result = (x - y);
-let men = "o resultado foi";
-console.log(m + result); // exibo m + r 
+let mens = "o resultado foi";
+console.log(mens + result); // exibo m + r 
